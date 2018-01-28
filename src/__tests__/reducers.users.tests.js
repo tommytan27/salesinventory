d9f11561fad46926d34cb4704e22a6fb8ccbcc8c
tommytan27 <tommy_tanzil@yahoo.com>
2018-01-28 13:34:18 +1100

    Fixed a typo

--- src/__tests__/reducers.users.tests.js
+++ src/__tests__/reducers.users.tests.js
@@ -1,8 +1,7 @@
-
 import users from './../reducers/users';
 
 describe('Users Store', () => {
-    it('should retun initial state of 3 users', () => {
+    it('should return initial state of 3 users', () => {
         expect(users(undefined, {})).toHaveLength(3);
     });
 });
