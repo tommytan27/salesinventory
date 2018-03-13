bc97b8b9731b690176330bfc439f170f14b79fe2
tommytan27 <tommy_tanzil@yahoo.com>
2018-03-13 21:07:57 +1100

    Refactor the sales only records to be usable for credit and stock records

--- src/containers/systemRecordTables/SystemSalesRecordTable.js
+++ src/containers/systemRecordTables/SystemSalesRecordTable.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
-import SalesRecordTable from './../../components/recordTables/SalesRecordTable';
+import SalesCreditStockRecordTable from './../../components/recordTables/SalesCreditStockRecordTable';
 import { openRecordsDetailsDialog } from './../../actions';
 
 const mapStateToProps = (state) => {
     return {
-        sales: state.sales,
+        records: state.sales,
         customers: state.customers,
         items: state.items
     };
@@ -12,12 +12,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSalesRecordClick: (salesDetails) => { dispatch(openRecordsDetailsDialog(salesDetails)) }
+        onRecordClick: (salesDetails) => { dispatch(openRecordsDetailsDialog(salesDetails)) }
     };
 }
 
 const SystemSalesRecordTable = connect(
     mapStateToProps, mapDispatchToProps
-)(SalesRecordTable);
+)(SalesCreditStockRecordTable);
 
 export default SystemSalesRecordTable;
